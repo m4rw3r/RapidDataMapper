d5990690ad9347825f9cc786af24f350d4a4c924
m4rw3r <m4rw3r@gmail.com>
2009-08-07 18:11:47 +0200

    Added MySQL driver

---
+++ lib/Db/Driver/Mysql/Connection.php
@@ -0,0 +1,157 @@
+<?php
+/*
+ * Created by Martin Wernståhl on 2009-03-13.
+ * Copyright (c) 2009 Martin Wernståhl.
+ * All rights reserved.
+ */
+
+/**
+ * 
+ */
+class Db_Driver_Mysql_Connection extends Db_Connection
+{
+	
+	protected $IDENT_CHAR = '`';
+	
+	protected function connect()
+	{
+		// try to connect
+		$conn = mysql_connect($this->hostname, $this->username, $this->password, $this->pconnect);
+		
+		// do we have a connection and can we select the database?
+		if($conn && mysql_select_db($this->database, $conn))
+		{
+			return $conn;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	
+	// ------------------------------------------------------------------------
+
+	protected function setCharset($charset, $collation)
+	{
+		// only raises warning if connection isn't connected, init_dbh() makes sure of that
+		// and because execute_sql() is protected, we can ignore the checking for dbh here
+		
+		// set charset on client side
+		mysql_set_charset($charset, $this->dbh);
+		
+		// set charset server side
+		return mysql_query("SET NAMES '".$this->escape_str($charset)."' COLLATE '".$this->escape_str($collation)."'", $this->dbh);
+	}
+	
+	// ------------------------------------------------------------------------
+
+	protected function executeSql($sql)
+	{
+		// only raises warning if connection isn't connected, init_dbh() makes sure of that
+		// and because execute_sql() is protected, we can ignore the checking for dbh here
+		return mysql_query($sql, $this->dbh);
+	}
+	
+	// ------------------------------------------------------------------------
+
+	public function error()
+	{
+		// dbh may not be loaded
+		// if condition is very much faster than error suppression with @
+		if( ! $this->dbh)
+		{
+			return "Database connection has not been established, error cannot be retrieved.";
+		}
+		else
+		{
+			return mysql_errno($this->dbh) . ": " . mysql_error($this->dbh);
+		}
+	}
+	
+	// ------------------------------------------------------------------------
+
+	public function insertId()
+	{
+		// dbh may not be loaded
+		// if condition is very much faster than error suppression with @
+		// (about 11% when dbh exists, otherwise over 90%)
+		if( ! $this->dbh)
+		{
+			return false;
+		}
+		else
+		{
+			return mysql_insert_id($this->dbh);
+		}
+	}
+    
+	// ------------------------------------------------------------------------
+    
+	public function affectedRows()
+	{
+		// dbh may not be loaded
+		// if condition is very much faster than error suppression with @
+		if( ! $this->dbh)
+		{
+			return false;
+		}
+		else
+		{
+			return mysql_affected_rows($this->dbh);
+		}
+	}
+	
+	// ------------------------------------------------------------------------
+	
+	public function version()
+	{
+		return $this->query("SELECT version() AS ver")->val();
+	}
+
+	// ------------------------------------------------------------------------
+
+	public function escapeStr($str, $like = false)
+	{
+		if(function_exists('mysql_real_escape_string') && is_resource($this->dbh))
+		{
+			// escape with regard to charset
+			$str = mysql_real_escape_string($str, $this->dbh);
+		}
+		elseif(function_exists('mysql_escape_string'))
+		{
+			// escape for MySQL
+			$str = mysql_escape_string($str);
+		}
+		else
+		{
+			// generic
+			$str = addslashes($str);
+		}
+		
+		if($like)
+		{
+			// replace LIKE-wildcards
+			$str = str_replace(array('%', '_'), array('\\%', '\\_'), $str);
+		}
+		
+		return $str;
+	}
+	
+	// ------------------------------------------------------------------------
+	
+	function _listTables()
+	{
+		return 'SHOW TABLES FROM '.$this->protect_identifiers($this->database);
+	}
+	
+	// ------------------------------------------------------------------------
+
+	public function _limit($str, $limit, $offset = false)
+	{
+		return $str . "\nLIMIT " . ($offset != false ?  $offset . ', ' : '') . $limit;
+	}
+}
+
+
+/* End of file db.php */
+/* Location: ./lib/drivers/mysql */
---
+++ lib/Db/Driver/Mysql/Result.php
@@ -0,0 +1,82 @@
+<?php
+/*
+ * Created by Martin Wernståhl on 2009-05-31.
+ * Copyright (c) 2009 Martin Wernståhl.
+ * All rights reserved.
+ */
+
+class Db_Driver_Mysql_Result extends Db_Result
+{
+	public function count()
+	{
+		return isset($this->num_rows) ? $this->num_rows : $this->num_rows = @mysql_num_rows($this->resource);
+	}
+	
+	public function seek($n)
+	{
+		return ($n >= $this->num_rows()) ? false : mysql_data_seek($this->resource, $n);
+	}
+	
+	public function freeResult()
+	{
+		if (is_resource($this->resource))
+		{
+			mysql_free_result($this->resource);
+			$this->resource = FALSE;
+		}
+	}
+	
+	public function next()
+	{
+		return mysql_fetch_object($this->resource);
+	}
+
+	public function nextArray()
+	{
+		return mysql_fetch_assoc($this->resource);
+	}
+	
+	public function metadata()
+	{
+		$ret = array();
+		// get encoding, so we can see if we need to correct for utf8
+		$charset = mysql_client_encoding($this->dbh);
+		$i = 0;
+		
+		while($field = mysql_fetch_field($this->resource))
+		{
+			$d = new stdClass();
+			
+			$d->name = $field->name;
+			$d->type = $field->type;
+			$d->default = $field->def;
+			
+			// get real length (eg. 45 for VARCHAR(45))
+			$d->length = mysql_field_len($this->resource, $i);
+			
+			// convert to boolean
+			$d->unsigned = $field->unsigned ? true : false;
+			$d->primary_key = $field->primary_key ? true : false;
+			
+			// correct for utf8, mysql returns the number of bytes - not characters as it should
+			if($charset == 'utf8' && in_array($d->type, array('string', 'blob')))
+			{
+				// mysql's utf8 blocks are 3 bytes in length
+				$d->length = $d->length / 3;
+			}
+			
+			// get auto_increment status:
+			$t = mysql_field_flags($this->resource, $i);
+			$d->auto_inc = strpos($t, 'auto_increment') !== false ? true : false;
+			
+			$ret[$d->name] = $d;
+			
+			$i++;
+		}
+		
+		return $ret;
+	}
+}
+
+/* End of file result.php */
+/* Location: ./lib/drivers/mysql */
