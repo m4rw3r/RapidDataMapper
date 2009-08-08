d784bb055e7664df19891722ee941d1b30f30a22
m4rw3r <m4rw3r@gmail.com>
2009-08-08 16:18:35 +0200

    Added more tests for the Db_Descriptor

--- tests/Db/DescriptorTest.php
+++ tests/Db/DescriptorTest.php
@@ -55,6 +55,131 @@ class Db_DescriptorTest extends PHPUnit_Framework_TestCase
 		
 		$desc->getFactory();
 	}
+	
+	// ------------------------------------------------------------------------
+
+	/**
+	 * @expectedException Db_Exception_Descriptor_MissingClassName
+	 */
+	public function testNoClassNameButSingular()
+	{
+		$desc = new Db_Descriptor();
+		$desc->setSingular('test');
+		
+		$desc->getClass();
+	}
+	public function testNoClassNameButSingular2()
+	{
+		$desc = new Db_Descriptor();
+		$desc->setSingular('test');
+		
+		// these build on singular
+		$this->assertEquals($desc->getSingular(), 'test');
+		$this->assertEquals($desc->getTable(), 'tests');
+	}
+	/**
+	 * @expectedException Db_Exception_Descriptor_MissingClassName
+	 */
+	public function testNoClassNameButSingular3()
+	{
+		$desc = new Db_Descriptor();
+		$desc->setSingular('test');
+		
+		$desc->getFactory();
+	}
+	
+	// ------------------------------------------------------------------------
+
+	/**
+	 * @expectedException Db_Exception_Descriptor_MissingClassName
+	 */
+	public function testNoClassNameButTable()
+	{
+		$desc = new Db_Descriptor();
+		$desc->setTable('tests');
+		
+		$desc->getClass();
+	}
+	/**
+	 * @expectedException Db_Exception_Descriptor_MissingClassName
+	 */
+	public function testNoClassNameButTable2()
+	{
+		$desc = new Db_Descriptor();
+		$desc->setTable('tests');
+		
+		$desc->getSingular();
+	}
+	public function testNoClassNameButTable3()
+	{
+		$desc = new Db_Descriptor();
+		$desc->setTable('tests');
+		
+		$this->assertEquals($desc->getTable(), 'tests');
+	}
+	/**
+	 * @expectedException Db_Exception_Descriptor_MissingClassName
+	 */
+	public function testNoClassNameButTable4()
+	{
+		$desc = new Db_Descriptor();
+		$desc->setTable('tests');
+		
+		$desc->getFactory();
+	}
+	
+	// ------------------------------------------------------------------------
+
+	/**
+	 * @expectedException Db_Exception_Descriptor_MissingClassName
+	 */
+	public function testNoClassNameButFactory()
+	{
+		$desc = new Db_Descriptor();
+		$desc->setFactory('new Foobar');
+		
+		$desc->getClass();
+	}
+	/**
+	 * @expectedException Db_Exception_Descriptor_MissingClassName
+	 */
+	public function testNoClassNameButFactory2()
+	{
+		$desc = new Db_Descriptor();
+		$desc->setFactory('new Foobar');
+		
+		$desc->getSingular();
+	}
+	/**
+	 * @expectedException Db_Exception_Descriptor_MissingClassName
+	 */
+	public function testNoClassNameButFactory3()
+	{
+		$desc = new Db_Descriptor();
+		$desc->setFactory('new Foobar');
+		
+		$desc->getTable();
+	}
+	public function testNoClassNameButFactory4()
+	{
+		$desc = new Db_Descriptor();
+		$desc->setFactory('new Foobar');
+		
+		$this->assertEquals($desc->getFactory(), 'new Foobar');
+	}
+	
+	// ------------------------------------------------------------------------
+
+	public function testSetClass()
+	{
+		$desc = new Db_Descriptor();
+		$desc->setClass('Test');
+		
+		$this->assertEquals($desc->getClass(), 'Test');
+		$this->assertEquals($desc->getSingular(), 'test');
+		$this->assertEquals($desc->getTable(), 'tests');
+		$this->assertEquals($desc->getFactory(), 'new Test');
+	}
 }
 
 
