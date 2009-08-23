e3c23034befea00e0f57f5ed4946bd74643f4b41
m4rw3r <m4rw3r@gmail.com>
2009-08-23 17:06:05 +0200

    Added exception for the event in which a query error occurs

---
+++ lib/Db/Exception/QueryError.php
@@ -0,0 +1,30 @@
+<?php
+/*
+ * Created by Martin Wernståhl on 2009-08-15.
+ * Copyright (c) 2009 Martin Wernståhl.
+ * All rights reserved.
+ */
+
+/**
+ * Exception for the event that a query error occurs.
+ */
+class Db_Exception_QueryError extends Db_Exception
+{
+	/**
+	 * The error message.
+	 * 
+	 * @var string
+	 */
+	public $error_message;
+	
+	function __construct($error_message)
+	{
+		parent::__construct('Query Error: '.$error_message.'.');
+		
+		$this->error_message = $error_message;
+	}
+}
+
+
+/* End of file QueryError.php */
+/* Location: ./lib/Db/Exception */
