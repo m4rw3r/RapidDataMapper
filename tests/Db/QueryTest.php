878396532522d4593da79b33a97ce7f9e68ca97f
m4rw3r <m4rw3r@gmail.com>
2009-09-04 13:40:43 +0200

    Added tests for nested where conditions (ie. with parenthesis) and bindWhere()

--- tests/Db/QueryTest.php
+++ tests/Db/QueryTest.php
@@ -183,6 +183,51 @@ class Db_QueryTest extends PHPUnit_Framework_TestCase
 	
 	// ------------------------------------------------------------------------
 	
+	public function testWhereNested()
+	{
+		$q = new Db_Query(new stdClass);
+		
+		$this->assertThat($q2 = $q->where(), $this->logicalNot($this->identicalTo($q)));
+		$this->assertTrue($q2 instanceof Db_Query);
+		
+		$this->assertEquals('(())', (String) $q);
+	}
+	public function testWhereNested2()
+	{
+		$mock = $this->getMock('Db_Connection', array('protectIdentifiers', 'escape'));
+		
+		$mock->expects($this->at(0))->method('protectIdentifiers')->with($this->equalTo('a'))->will($this->returnValue('"a"'));	
+		$mock->expects($this->at(1))->method('escape')->with($this->equalTo('b'))->will($this->returnValue('b_result'));
+		
+		$q = new Db_Query($mock);
+		
+		$q->where('a', 'b');
+		$this->assertThat($q2 = $q->where(), $this->logicalNot($this->identicalTo($q)));
+		$this->assertTrue($q2 instanceof Db_Query);
+		
+		$this->assertEquals('("a" = b_result AND ())', (String) $q);
+	}
+	public function testWhereNested3()
+	{
+		$mock = $this->getMock('Db_Connection', array('protectIdentifiers', 'escape'));
+		
+		$mock->expects($this->at(0))->method('protectIdentifiers')->with($this->equalTo('a'))->will($this->returnValue('"a"'));	
+		$mock->expects($this->at(1))->method('escape')->with($this->equalTo('b'))->will($this->returnValue('b_result'));
+		$mock->expects($this->at(2))->method('protectIdentifiers')->with($this->equalTo('c'))->will($this->returnValue('"c"'));	
+		$mock->expects($this->at(3))->method('escape')->with($this->equalTo('d'))->will($this->returnValue('d_result'));
+		
+		$q = new Db_Query($mock);
+		
+		$q->where('a', 'b');
+		$this->assertThat($q2 = $q->where(), $this->logicalNot($this->identicalTo($q)));
+		$this->assertTrue($q2 instanceof Db_Query);
+		$this->assertSame($q2, $q2->where('c', 'd'));
+		
+		$this->assertEquals('("a" = b_result AND ("c" = d_result))', (String) $q);
+	}
+		
+	// ------------------------------------------------------------------------
+	
 	public function testWhereOr()
 	{
 		$mock = $this->getMock('Db_Connection', array('protectIdentifiers', 'escape'));
@@ -279,6 +324,121 @@ class Db_QueryTest extends PHPUnit_Framework_TestCase
 		$this->assertEquals('("a" = (subquery_a))', (String) $q);
 		$this->assertSame($q, $q->where('or b', $sub2));
 		$this->assertEquals('("a" = (subquery_a) OR "b" = (subquery_b))', (String) $q);
+	}
+	
+	// ------------------------------------------------------------------------
+	
+	public function testWhereOrNested()
+	{
+		$q = new Db_Query(new stdClass);
+		
+		$this->assertThat($q2 = $q->where('or'), $this->logicalNot($this->identicalTo($q)));
+		$this->assertTrue($q2 instanceof Db_Query);
+		
+		$this->assertEquals('(())', (String) $q);
+	}
+	public function testWhereOrNested2()
+	{
+		$mock = $this->getMock('Db_Connection', array('protectIdentifiers', 'escape'));
+		
+		$mock->expects($this->at(0))->method('protectIdentifiers')->with($this->equalTo('a'))->will($this->returnValue('"a"'));	
+		$mock->expects($this->at(1))->method('escape')->with($this->equalTo('b'))->will($this->returnValue('b_result'));
+		
+		$q = new Db_Query($mock);
+		
+		$q->where('a', 'b');
+		$this->assertThat($q2 = $q->where('or'), $this->logicalNot($this->identicalTo($q)));
+		$this->assertTrue($q2 instanceof Db_Query);
+		
+		$this->assertEquals('("a" = b_result OR ())', (String) $q);
+	}
+	public function testWhereOrNested3()
+	{
+		$mock = $this->getMock('Db_Connection', array('protectIdentifiers', 'escape'));
+		
+		$mock->expects($this->at(0))->method('protectIdentifiers')->with($this->equalTo('a'))->will($this->returnValue('"a"'));	
+		$mock->expects($this->at(1))->method('escape')->with($this->equalTo('b'))->will($this->returnValue('b_result'));
+		$mock->expects($this->at(2))->method('protectIdentifiers')->with($this->equalTo('c'))->will($this->returnValue('"c"'));	
+		$mock->expects($this->at(3))->method('escape')->with($this->equalTo('d'))->will($this->returnValue('d_result'));
+		
+		$q = new Db_Query($mock);
+		
+		$q->where('a', 'b');
+		$this->assertThat($q2 = $q->where('or'), $this->logicalNot($this->identicalTo($q)));
+		$this->assertTrue($q2 instanceof Db_Query);
+		$this->assertSame($q2, $q2->where('c', 'd'));
+		
+		$this->assertEquals('("a" = b_result OR ("c" = d_result))', (String) $q);
+	}
+	
+	// ------------------------------------------------------------------------
+	
+	public function testBindWhere()
+	{
+		$mock = $this->getMock('Db_Connection', array('replaceBinds'));
+		
+		$mock->expects($this->once())->method('replaceBinds')->with($this->equalTo('a = ?'), $this->equalTo(array('foobar')))->will($this->returnValue('foobar_result'));
+		
+		$q = new Db_Query($mock);
+		
+		$this->assertSame($q, $q->bindWhere('a = ?', array('foobar')));
+		
+		$this->assertEquals('(foobar_result)', (String) $q);
+	}
+	
+	// ------------------------------------------------------------------------
+	
+	public function testBindWhereMultiple()
+	{
+		$mock = $this->getMock('Db_Connection', array('replaceBinds'));
+		
+		$mock->expects($this->at(0))->method('replaceBinds')->with($this->equalTo('a = ?'), $this->equalTo(array('foobar')))->will($this->returnValue('foobar_result'));
+		$mock->expects($this->at(1))->method('replaceBinds')->with($this->equalTo('b = ?'), $this->equalTo(array('bar', 'lol')))->will($this->returnValue('bar_lol'));
+		
+		$q = new Db_Query($mock);
+		
+		$this->assertSame($q, $q->bindWhere('a = ?', array('foobar')));
+		
+		$this->assertEquals('(foobar_result)', (String) $q);
+		
+		$this->assertSame($q, $q->bindWhere('b = ?', array('bar', 'lol')));
+		
+		$this->assertEquals('(foobar_result AND bar_lol)', (String) $q);
+	}
+	
+	// ------------------------------------------------------------------------
+	
+	public function testBindWhereOr()
+	{
+		$mock = $this->getMock('Db_Connection', array('replaceBinds'));
+		
+		$mock->expects($this->once())->method('replaceBinds')->with($this->equalTo('a = ?'), $this->equalTo(array('foobar')))->will($this->returnValue('foobar_result'));
+		
+		$q = new Db_Query($mock);
+		
+		$this->assertSame($q, $q->bindWhere('or a = ?', array('foobar')));
+		
+		$this->assertEquals('(foobar_result)', (String) $q);
+	}
+	
+	// ------------------------------------------------------------------------
+	
+	public function testBindWhereOrMultiple()
+	{
+		$mock = $this->getMock('Db_Connection', array('replaceBinds'));
+		
+		$mock->expects($this->at(0))->method('replaceBinds')->with($this->equalTo('a = ?'), $this->equalTo(array('foobar')))->will($this->returnValue('foobar_result'));
+		$mock->expects($this->at(1))->method('replaceBinds')->with($this->equalTo('b = ?'), $this->equalTo(array('bar', 'lol')))->will($this->returnValue('bar_lol'));
+		
+		$q = new Db_Query($mock);
+		
+		$this->assertSame($q, $q->bindWhere('or a = ?', array('foobar')));
+		
+		$this->assertEquals('(foobar_result)', (String) $q);
+		
+		$this->assertSame($q, $q->bindWhere('or b = ?', array('bar', 'lol')));
+		
+		$this->assertEquals('(foobar_result OR bar_lol)', (String) $q);
 	}
 }
 
