9c4d2bc16358739de8707139f03c75124edb947e
m4rw3r <m4rw3r@gmail.com>
2010-04-03 23:37:07 +0200

    Added class builder for <Class>CollectionFilter

---
+++ lib/Rdm/Builder/CollectionFilter.php
@@ -0,0 +1,22 @@
+<?php
+/*
+ * Created by Martin Wernståhl on 2010-04-02.
+ * Copyright (c) 2010 Martin Wernståhl.
+ * All rights reserved.
+ */
+
+/**
+ * 
+ */
+class Rdm_Builder_CollectionFilter extends Rdm_Util_Code_ClassBuilder
+{
+	public function __construct(Rdm_Descriptor $desc)
+	{
+		$this->setClassName($desc->getClass().'CollectionFilter');
+		$this->setExtends('Rdm_Collection_Filter');
+	}
+}
+
+
+/* End of file Collection.php */
+/* Location: ./lib/Rdm/Builder */
--- lib/Rdm/Builder/Main.php
+++ lib/Rdm/Builder/Main.php
@@ -13,6 +13,7 @@ class Rdm_Builder_Main extends Rdm_Util_Code_Container
 	public function __construct(Rdm_Descriptor $descriptor)
 	{
 		$this->addPart(new Rdm_Builder_Collection($descriptor));
+		$this->addPart(new Rdm_Builder_CollectionFilter($descriptor));
 	}
 	
 	// ------------------------------------------------------------------------
