7dd940da5d3dff4c64c9d50f08eb65493e2208b3
m4rw3r <m4rw3r@gmail.com>
2010-05-03 17:32:39 +0200

    Added missing Rdm_Adapter_ConnectionException

---
+++ lib/Rdm/Adapter/ConnectionException.php
@@ -0,0 +1,42 @@
+<?php
+/*
+ * Created by Martin Wernståhl on 2009-08-07.
+ * Copyright (c) 2009 Martin Wernståhl.
+ * All rights reserved.
+ */
+
+/**
+ * Exception for the event that a database connection error occurs.
+ */
+class Rdm_Adapter_ConnectionException extends Exception implements Rdm_Exception
+{
+	/**
+	 * The error message.
+	 * 
+	 * @var string
+	 */
+	protected $error_message;
+	
+	function __construct($error_message)
+	{
+		parent::__construct('Connection Error: "'.$error_message.'".');
+		
+		$this->error_message = $error_message;
+	}
+	
+	// ------------------------------------------------------------------------
+
+	/**
+	 * Returns the error message.
+	 * 
+	 * @return string
+	 */
+	public function getErrorMessage()
+	{
+		return $this->error_message;
+	}
+}
+
+
+/* End of file ConnectionException.php */
+/* Location: ./lib/Rdm/Adapter */
